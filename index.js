76585ca6f4f0dedb10034f7b22649712eb7bdc78
albertlai <albertlai@gmail.com>
2013-05-10 22:52:15 -0700

    refactored wall logic to a separate module

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var cluster = require('cluster');
 var io = require('socket.io').listen(server);
-var ejs = require('ejs');
-var fn = require('./static/fn');
+var cluster = require('cluster');
+var pixelwall = require('./pixelwall');
 
-var RedisStore = require('socket.io/lib/stores/redis')
-, redis  = require('socket.io/node_modules/redis')
-, pub    = redis.createClient()
-, sub    = redis.createClient()
-, client = redis.createClient();
+var RedisStore = require('socket.io/lib/stores/redis');
+var redis  = require('socket.io/node_modules/redis');
+var pub    = redis.createClient();
+var sub    = redis.createClient();
+var client = redis.createClient();
 
+client.on('error', function (err) {
+    console.log("Redis Error " + err);
+});
+    
+io.set('log level', 1);
 // Setup redis
 io.set('store', new RedisStore({
             redisPub : pub, 
@@ -19,112 +23,39 @@ io.set('store', new RedisStore({
             redisClient : client
 }));
 
-client.on('error', function (err) {
-    console.log("Error " + err);
-});
+// TODO: log to S3 and playback
 
-
-/*      n
-     o o o o
-  m   o o o
-     o o o o    
-*/
-var m = 24,  n = 12;
-
-function getZeroedState() {
-    var arr = [];
-    var len = Math.ceil(m * n / 32);
-    for (var i = 0; i < len; i++) {
-        arr.push(0);
-    }
-    return arr;
-}
-    
-if (cluster.isMaster) {
-    // Initialize
-    client.get('state', function (err, obj) {
-       if (!obj) {        
-           client.set('state', getZeroedState());
-       }
-    });
-    
+if (cluster.isMaster) { 
+    pixelwall.initialize(client);
+    client.set('num_connections', 0);
     // Fork workers.
     var numCPUs = require('os').cpus().length;
     for (var i = 0; i < numCPUs; i++) {
         cluster.fork();
-    }
-    
+    }    
     cluster.on('exit', function(worker, code, signal) {
         console.log('worker ' + worker.process.pid + ' died');
     });
 
 } else {    
     console.log('forked');
-
-    // Setup the values for lattice coordinates (in miller indices)
-    // All units are in % of the total width of the lattice
-    var w = 4;
-    var d_10 = (100 - w) / (n-1);
-    var d_01 = d_10 / Math.sqrt(3);
-    var d_m = d_01 / 2;
-    var d_n = d_10;    
-    var height = m * d_m + w / 2;
-
-    // rescale to be a % of height
-    d_m = d_m * 100 / height; 
-    var h = w * 100 / height; 
-
-    // Colors
-    var dim = "#A4A49F";
-    var light = "#FFD659";
-
     function get(req, res) {
-        var context = {
-            initial_state: getZeroedState(),
-            height: height,
-            m: m,
-            n: n, 
-            w: w,
-            h: h,
-            d_m: d_m,
-            d_n: d_n,
-            dim: dim,
-            light: light
-        };
+        context = pixelwall.getContext();
         res.render('index.html', context);   
     };   
-
-    // Update the state with data from client. Data comes in with the form:
-    //       0 | (1 << index) if we are lighting up a circle (eg 01000000)
-    // or   
-    //    ~ (0 | (1 << index)) if we are dimming a circle (eg 11111011)
-    function tap(data) {
-        console.log('tap: ' + data);
-        var light = fn.accumulate(data, fn.and) == 0;
-        var process = function (err, state) {
-            state = state.split(',');
-            // If it is lighting up, we simply || with the state
-            // If it is dimming, we simply && with the state
-            state = fn.map(state, data, light ? fn.or : fn.and);        
-            console.log('state: ' + state);
-            io.sockets.emit('update', state);
-            client.set('state', state);
-        };
-        client.get('state', process);
-    }
-
-
-    io.on('connection', function (socket) {
-        socket.on('tap', tap);       
-        client.get('state', function(err, state) {
-            state = state.split(',');
-            socket.emit('update', state);
-        });
-    });
-
-    app.engine('html', ejs.renderFile);
+    app.engine('html', require('ejs').renderFile);
     app.use('/static', express.static( __dirname + '/static'));
     app.get('/', get);   
+    
+    var conn_count = 0;
+    io.on('connection', pixelwall.onConnection(client, io, function(socket) {
+            conn_count++;
+            client.incr('num_connections');
+            socket.on('disconnect', function(socket) {
+                    client.decr('num_connections');
+            });
+                
+    }));
 
     server.listen(8080);
 }
---
+++ pixelwall.js
@@ -0,0 +1,107 @@
+// All the logic for getting / updating the pixel wall 
+var fn = require('./static/fn');
+
+/*      n
+     o o o o
+  m   o o o
+     o o o o    
+*/
+var m = 24,  n = 12;
+var STATE = 'pixelwall::state';
+
+function getZeroedState() {
+    var arr = [];
+    var len = Math.ceil(m * n / 32);
+    for (var i = 0; i < len; i++) {
+        arr.push(0);
+    }
+    return arr;
+}
+
+// Initialize the database
+function initialize(client) {
+    // Initialize
+    client.get(STATE, function (err, obj) {
+        if (!obj) {        
+            client.set(STATE, getZeroedState());
+        }
+    });
+}
+
+// Setup the values for lattice coordinates (in miller indices)
+// All units are in % of the total width of the lattice
+var w = 4;
+var d_10 = (100 - w) / (n-1);
+var d_01 = d_10 / Math.sqrt(3);
+var d_m = d_01 / 2;
+var d_n = d_10;    
+var height = m * d_m + w / 2;
+// rescale to be % of height
+d_m = d_m * 100 / height; 
+var h = w * 100 / height; 
+
+// Colors
+var dim = "#A4A49F";
+var light = "#FFD659";
+
+function getContext() {
+    var context = {
+        initial_state: getZeroedState(),
+        height: height,
+        m: m,
+        n: n, 
+        w: w,
+        h: h,
+        d_m: d_m,
+        d_n: d_n,
+        dim: dim,
+        light: light
+    };
+    return context;
+};   
+
+function getTap(client, io) {    
+    // Update the state with data from client. Data comes in with the form:
+    //       0 | (1 << index) if we are lighting up a circle (eg 01000000)
+    // or   
+    //    ~ (0 | (1 << index)) if we are dimming a circle (eg 11111011)
+    var tap = function (data) {
+        console.log(new Date() + ': pixelwall-tap: ' + data);
+        var light = fn.accumulate(data, fn.and) == 0;
+        var process = function (err, state) {
+            state = state.split(',');
+            // If it is lighting up, we simply || with the state
+            // If it is dimming, we simply && with the state
+            state = fn.map(state, data, light ? fn.or : fn.and);        
+            console.log(new Date() + ': pixelwall-state: ' + state);
+            io.sockets.emit('pixelwall_update', state);
+            client.set(STATE, state);
+        };
+        client.get(STATE, process);
+    };
+    return tap;
+}
+
+// Hook up the pixelwall_tap event as well as update the client with 
+// the latest state on connection
+function onConnection(client, io, fn) {
+    var tap = getTap(client, io);
+    var connection = function (socket) {
+            socket.on('pixelwall_tap', tap);       
+            client.get(STATE, function(err, state) {
+                    state = state.split(',');
+                    socket.emit('pixelwall_update', state);
+            });
+            if (fn) {
+                fn(socket);
+            }
+        };
+    return connection;
+}
+
+
+
+exports.initialize = initialize;
+exports.getContext = getContext;
+exports.onConnection = onConnection;
+
